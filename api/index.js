78012ee6f428c5341905b53dc12b3d4eb723c686
Debapriya Das <76766443+Debapriya-source@users.noreply.github.com>
2022-10-18 13:01:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Article = require("./models/article");
-const articleRouter = require("./routes/articles");
+const Article = require("../models/article");
+const articleRouter = require("../routes/articles");
 const methodOverride = require("method-override");
 const app = express();
 
